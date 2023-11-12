a49668df3c952871e53a9e71aa12b8e43992f627
Yusuf Emre ÖZDEN <112346866+yusufemreozden@users.noreply.github.com>
2023-11-12 14:40:07 +0300

    Update digitRange.c

--- digitRange.c
+++ digitRange.c
@@ -4,7 +4,6 @@
 /*   Digit Range                                                              */
 /*                                                                            */
 /*   By: Yusuf Emre OZDEN | <yusufemreozdenn@gmail.com>                       */
-/*   Student ID: 200030087                                                    */
 /*                                                                            */      
 /*   https://GitHub.com/yusufemreozden                                        */
 /*   https://linkedIn.com/in/yusufemreozden                                   */
